4d9a71bc58e97b476692568305e259d36f3c6f36
MrLiauw <maryantoliauw@gmail.com>
2016-05-04 23:46:48 +0800

    Adding test 3 book1 and 1 book2 pass

--- HomeworkTests/CartTests.cs
+++ HomeworkTests/CartTests.cs
@@ -144,5 +144,26 @@ namespace Homework.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void CalculatePrice_3_Book1_1_Book2_Should_Return_390()
+        {
+            //arrange
+            var target = new Cart();
+            var bookList = new List<Book>() { 
+                new Book() { id = 1}, 
+                new Book() { id = 1},
+                new Book() { id = 1},
+                new Book() { id = 2}
+            };
+            var pricePerBook = 100;
+
+            var expected = 390;
+
+            //act
+            var actual = target.CalculatePrice(bookList, pricePerBook);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
